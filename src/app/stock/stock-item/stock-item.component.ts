bae5e674b133a578dfd6db7b713ecdae148ce4de
victor-arana <victor_arana@hotmail.com>
2020-08-16 18:56:28 -0500

    :sparkle: use ngFor directive

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -8,17 +8,21 @@ import { Stock } from '../../model/stock';
 })
 export class StockItemComponent implements OnInit {
 
-  public stock: Stock;
+  public stocks: Array<Stock>;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+    this.stocks = [
+      new Stock('Test Stock Company', 'TSC', 85, 80),
+      new Stock('Second Stock Company', 'SSC', 10, 20),
+      new Stock('Last Stock Company', 'LSC', 876, 765)
+    ];
   }
 
-  toggleFavorite(event){
-    console.log('We are toggling the favorite state for this stock', event);
-    this.stock.favorite = !this.stock.favorite;
+  toggleFavorite(event, index){
+    console.log('We are toggling the favorite state for stock', index, event);
+    this.stocks[index].favorite = !this.stocks[index].favorite;
   }
 
 }
