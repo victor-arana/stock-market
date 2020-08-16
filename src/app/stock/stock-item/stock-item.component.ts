e991bb83f78faabd1920e7df20cff43a050ee2c7
victor-arana <victor_arana@hotmail.com>
2020-08-15 20:12:33 -0500

    using ngStyle

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -9,7 +9,7 @@ import { Stock } from '../../model/stock';
 export class StockItemComponent implements OnInit {
 
   public stock: Stock;
-  public stockClasses;
+  public stockStyles;
 
   constructor() { }
 
@@ -17,11 +17,9 @@ export class StockItemComponent implements OnInit {
     this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
     let diff = (this.stock.price / this.stock.previousPrice) - 1;
     let largeChange = Math.abs(diff) > 0.01;
-    this.stockClasses  = {
-      "positive": this.stock.isPositiveChange(),
-      "negative": !this.stock.isPositiveChange(),
-      "large-change": largeChange,
-      "small-change": !largeChange
+    this.stockStyles  = {
+      "color": this.stock.isPositiveChange() ? "green" : "red",
+      "font-size": largeChange ? "1.2em" : "0.8em"
     };
   }
 
