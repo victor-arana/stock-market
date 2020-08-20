f6e8fe4862b7bd33dbba524e485471808de84481
victor-arana <victor_arana@hotmail.com>
2020-08-20 12:16:49 -0500

    :recycle: cleanup, remove unused imports

--- src/app/stock/create-stock/create-stock.component.ts
+++ src/app/stock/create-stock/create-stock.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-create-stock',
   templateUrl: './create-stock.component.html',
   styleUrls: ['./create-stock.component.css']
 })
-export class CreateStockComponent implements OnInit {
+export class CreateStockComponent {
 
   constructor() { }
 
