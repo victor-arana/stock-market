4216e286f3db5ad56fd393b83d77122712d7c258
victor-arana <victor_arana@hotmail.com>
2020-08-24 14:52:29 -0500

    :recycle: refactor, display app-stock-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,17 @@
-import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
-import { Stock } from './model/stock';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnChanges{
+export class AppComponent implements OnInit{
   
   title = 'stock-market';
-
-  public stock: Stock;
-
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log('App Component - On Changes - ', changes);    
-  }
-
+  
   ngOnInit(): void{
-    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80, 'NASDAQ');
+    
   }
 
-  onToggleFavorite(stock: Stock){
-    console.log('Favorite for stock ', stock, ' was triggered');
-    this.stock.favorite = !this.stock.favorite;
-  }
+  
 }
