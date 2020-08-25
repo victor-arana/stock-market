e96a39f52ea15d173c89f0e368fcf316daa0c147
victor-arana <victor_arana@hotmail.com>
2020-08-25 13:29:20 -0500

    :sparkles: service, use messaging service

--- src/app/stock/create-stock/create-stock.component.ts
+++ src/app/stock/create-stock/create-stock.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Stock } from 'src/app/model/stock';
 import { StockService } from 'src/app/services/stock.service';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-create-stock',
@@ -11,10 +12,10 @@ export class CreateStockComponent {
 
   public stock: Stock;
   public confirmed = false;
-  public message = null;
   public exchanges = ['NYSE', 'NASDAQ', 'OTHER'];
 
-  constructor(private stockService: StockService) { 
+  constructor(private stockService: StockService,
+              public messageService: MessageService) { 
     this.stock = new Stock('test', '', 0, 0, 'NASDAQ');
   }
 
@@ -28,11 +29,12 @@ export class CreateStockComponent {
     if(stockForm.valid){
       let created = this.stockService.createStock(this.stock);
       if(created){
-        this.message = 'Successfully created stock with stock code: '
+        this.messageService.message =
+          'Successfully created stock with stock code: '
           + this.stock.code;
       } else{
-        this.message = 'Stock with stock code: ' + this.stock.code
-          + ' already exists';
+        this.messageService.message = 'Stock with stock code: ' 
+          + this.stock.code + ' already exists';
       }
     } else{
       console.error('Stock form is in an invalid state');
