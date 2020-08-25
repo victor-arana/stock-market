abc657020002960c23e7611d76323e1926b15cfc
victor-arana <victor_arana@hotmail.com>
2020-08-24 21:05:52 -0500

    :sparkles: inject-service, uses the service

--- src/app/stock/stock-list/stock-list.component.ts
+++ src/app/stock/stock-list/stock-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Stock } from 'src/app/model/stock';
+import { StockService } from 'src/app/services/stock.service';
 
 @Component({
   selector: 'app-stock-list',
@@ -10,14 +11,10 @@ export class StockListComponent implements OnInit {
 
   public stocks: Stock[];
   
-  constructor() { }
+  constructor(private stockService: StockService) { }
 
   ngOnInit(): void {
-    this.stocks = [
-      new Stock('Test Stock Company', 'TSC', 85, 80, 'NASDAQ'),
-      new Stock('Second Stock Company', 'SSC', 10, 20, 'NSE'),
-      new Stock('Last Stock Company', 'LSC', 867, 765, 'NYSE')
-    ]
+    this.stocks = this.stockService.getStocks();
   }
 
   onToggleFavorite(stock: Stock){
